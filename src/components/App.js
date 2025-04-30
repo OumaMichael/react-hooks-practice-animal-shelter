d4f851898378a6bc6539f5bb5766fa05377c5939
MICHAEL OUMA <oumamichael108@gmail.com>
2025-04-30 22:04:51 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
@@ -7,21 +6,36 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handleChangeType(type) {
+    setFilters({ type });
+  }
+
+  function handleFindPetsClick() {
+    let url = "http://localhost:3001/pets";
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`;
+    }
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPets(data));
+  }
+
+  function handleAdoptPet(id) {
+    const updatedPets = pets.map((pet) =>
+      pet.id === id ? { ...pet, isAdopted: true } : pet
+    );
+    setPets(updatedPets);
+  }
+
   return (
-    <div className="ui container">
-      <header>
-        <h1 className="ui dividing header">React Animal Shelter</h1>
-      </header>
-      <div className="ui container">
-        <div className="ui grid">
-          <div className="four wide column">
-            <Filters />
-          </div>
-          <div className="twelve wide column">
-            <PetBrowser />
-          </div>
-        </div>
-      </div>
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">🐾 Animal Shelter</h1>
+      <Filters
+        onChangeType={handleChangeType}
+        onFindPetsClick={handleFindPetsClick}
+      />
+      <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
     </div>
   );
 }
