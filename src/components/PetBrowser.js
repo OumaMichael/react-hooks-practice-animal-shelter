2bb398aa1fc05d6eba61ee822f272fb1dd022836
MICHAEL OUMA <oumamichael108@gmail.com>
2025-04-30 22:01:50 +0300

    Update PetBrowser.js

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,16 @@
 import React from "react";
-
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({ pets, onAdoptPet }) {
+  return (
+    <div className="row">
+      {pets.map((pet) => (
+        <div className="col-md-4 mb-4" key={pet.id}>
+          <Pet pet={pet} onAdoptPet={onAdoptPet} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default PetBrowser;
